52ab31f7b5479d0a868eb3c3adbb2c9b68fffff2
syuilo <syuilotan@yahoo.co.jp>
2019-06-01 12:30:58 +0900

    :art:

--- src/client/app.vue
+++ src/client/app.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
 	<header class="ui-header">Kiwi</header>
-	<nav>
+	<nav class="ui-nav">
 		<ul>
 			<li><router-link to="/:new" v-t="'create-page'"></router-link></li>
 		</ul>
 	</nav>
-	<main>
+	<main class="ui-main">
 		<router-view></router-view>
 	</main>
 </div>
@@ -20,11 +20,26 @@ export default Vue.extend({});
 <style lang="scss">
 .ui-header {
 	position: fixed;
-	z-index: 1000;
+	z-index: 1001;
 	top: 0;
 	left: 0;
 	width: 100%;
 	line-height: 48px;
 	box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 8px;
+	background: #fff;
+}
+
+.ui-nav {
+	position: fixed;
+	z-index: 1000;
+	top: 48px;
+	left: 0;
+	width: 300px;
+	height: calc(100% - 48px);
+	background: #e2e2e2;
+}
+
+.ui-main {
+	margin: 48px 0 0 300px;
 }
 </style>
